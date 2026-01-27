47af6a958c74e9179140c29dad2dc3b98f5fa523
ivy <119665497+lvlvllvlvllvlvl@users.noreply.github.com>
2026-01-27 20:48:43 +0700

    fix and run test

--- packages/tests/src/App.tsx
+++ packages/tests/src/App.tsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { useNavigation } from "use-navigation-api";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const url = useNavigation();
 
   return (
     <>
@@ -22,6 +24,9 @@ function App() {
           count is {count}
         </button>
         <p>
+          URL: <code id="navigation-url">{url.toString()}</code>
+        </p>
+        <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
@@ -29,7 +34,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
